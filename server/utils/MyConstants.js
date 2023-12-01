67e0473d71484e3f1b37b3b4c14d6f9bdbcfc096
DeleteGitHub058 <127474910+DeleteGitHub058@users.noreply.github.com>
2023-12-01 15:07:07 +0700

    upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.djujsfz.mongodb.net',
+    DB_USER: 'tanaone0406',
+    DB_PASS: '1234',
+    DB_DATABASE: 'T1ConShop',
+    // DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'SundayTheKingPlay',
+    JWT_EXPIRES: '360000000', // in milliseconds
+    EMAIL_USER: 'lamnhuttan911@gmail.com', // gmail service
+    EMAIL_PASS: 'gfrcnhpkseajxecf'
+  };
+  module.exports = MyConstants;
